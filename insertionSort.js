09c134d5909ea1ed481819d033ce7355464b353d
saul <slbrtg@gmail.com>
2017-08-24 16:28:04 -0700

    update spacing

--- insertionSort.js
+++ insertionSort.js
@@ -1,36 +1,64 @@
+const array = [5,4,3,2,1];
+
 // INSERTION SORT
-var array = [9, 2, 5,7];
-
 function insertionSort(array) {
-  for(var i = 0; i < array.length; i++) {
-  console.log("i = " + i);
+  for(let index = 0; index < array.length; index++) {
+    let storedValue = array[index];
+    let sorter = index - 1;
+    while (sorter >= 0 && array[sorter] > storedValue) {
+      array[sorter + 1] = array[sorter];
+      sorter--;
+    }
+    array[sorter + 1] = storedValue;
+  }
+  return array;
+}
 
-    var temp = array[i];
-    console.log("temp1 = " + array[i]);
 
-    var j = i - 1;
-    console.log("j = "+j);
 
-    if(j < 0 ){
-    console.log("j is less than zero, will not enter while loop");
+// INSERTION SORT WITH CONSOLE LOGS
+function insertionSortWithLogs(array) {
+  for(let index = 0; index < array.length; index++) {
+  console.log("LOOP "+index);
+  console.log("index = " + index);
+
+    let storedValue = array[index];
+    console.log("storedValue1 = " + array[index]);
+
+    let sorter = index - 1;
+    console.log("sorter = "+sorter);
+
+    if(sorter < 0 ){
+    console.log("sorter is less than zero, will not enter while loop");
     } else {
-    console.log("j is equal to or greater than zero, entering while loop");
+    console.log("sorter is equal to or greater than zero, entering while loop");
     }
-    while (j >= 0 && array[j] > temp) {
-    	console.log("begin while loop");
 
-      array[j + 1] = array[j];
-      console.log("array["+(j+1)+"] =  " + array[j]);
+    // while loop runs if sorter is greater than or equal to 0 &&
+    // the captured storedValue value is less than the current array value
+    while (sorter >= 0 && array[sorter] > storedValue) {
+      console.log("");
+    	console.log("while loop");
+
+      // captures the value +1 of where it begins and assigns it to its current index
+      array[sorter + 1] = array[sorter];
+      console.log("array["+(sorter+1)+"] =  " + array[sorter]);
       console.log()
-      j--;
-    }
-    console.log("array leaves while (loop #"+i+") = "+ array);
 
-    array[j + 1] = temp;
-    console.log("array after pushing in temp " + array);
+      sorter--;
+      console.log("array in while loop " + array);
+      console.log("");
+    }
+    console.log("Exited while loop")
+    console.log("current state of the array = "+ array);
+
+    // Pushes the stored value to the index +1 from where it ended the while loop
+    //
+    array[sorter + 1] = storedValue;
+    console.log("array after pushing in the storedValue " + array);
     console.log("");
   }
   return array;
 }
 
-console.log(insertionSort(array));
+console.log(insertionSortWithLogs(array));
